6b2f384cb50a3c2ca7c93eb370fe8418b223d3f3
Lakshay-sethi <58126894+Lakshay-sethi@users.noreply.github.com>
2023-09-03 04:13:23 -0700

    utils file updated, tutorial added

--- src/delta_e/utils.py
+++ src/delta_e/utils.py
@@ -7,18 +7,26 @@ from typing import Union
 class FileReader:
     """Reads in raw data."""
 
-    def __init__(self, filepath: Union[str,Path]) -> None:
+    def __init__(self, filepath: Union[str,Path], isUrl = False) -> None:
         """Parses filepath and extension.
         
         Args:
             filepath: Union[str,Path]
                 Location of file to read in. 
+            isUrl : Bool
+                whether the file path points to a local link or an internet location.
         """
-        if type(filepath) == str:
+        self.isUrl = isUrl
+        self.ext = ""
+        
+        if type(filepath) == str and isUrl == False :
             self.file_path = Path(filepath)
+            self.ext = filepath.suffix
+        elif type(filepath) != str and isUrl == False:
+            self.file_path = filepath
+            self.ext = filepath.suffix
         else:
             self.file_path = filepath
-        self.ext = filepath.suffix
     
     def get_data(self) -> pd.DataFrame:
         """Reads data to a dataframe
@@ -26,12 +34,14 @@ class FileReader:
         
         # check if file has been locally downloaded 
         try:
-            if self.ext == '.csv':
+            if self.ext == '.csv' and self.isUrl == False :
                 data = pd.read_csv(Path(
                     Path(__file__).parent,
                     "resources",
                     str(self.file_path))
                 )
+            elif self.isUrl == True:
+                 data = pd.read_csv(self.file_path)
             else: 
                 raise NotImplementedError(
                     f"Extension {self.ext} can not be read."
